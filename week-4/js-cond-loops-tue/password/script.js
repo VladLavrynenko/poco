f39bcb79e4c41dc2537e6ae6da1ff781df133647
Vlad <vlad.lavrynenko@powercoders.org>
2024-04-25 22:53:22 +0200

    Generate Password. Added a new function which generate passwords with some constraints. Works with the function checkPassword().

--- week-4/js-cond-loops-tue/password/script.js
+++ week-4/js-cond-loops-tue/password/script.js
@@ -8,7 +8,9 @@
 // 1 uppercase
 // 1 special
 
-let input = prompt("Enter your password.");
+
+// BEGIN
+// let input = prompt("Enter your password.");
 
 function checkPassword(_password) {
     const password = _password;
@@ -18,8 +20,10 @@ function checkPassword(_password) {
 
     const isUpperCase = str => str === str.toUpperCase();
     const isLowerCase = str => str === str.toLowerCase();
+    
+    console.log("length: " + password.toString());
 
-    if(password.length >= 4){
+    if(password.length >= 4) {
         constraints[0] = true;
     }
 
@@ -50,4 +54,80 @@ function checkPassword(_password) {
     console.log(constraints.toString())
 }
 
-checkPassword(input);
+// checkPassword(input);
+
+
+
+
+
+
+
+
+//Generate valid password 
+//The same constraints:
+// 4 minimum length
+// 1 smallcase (97-122)
+// 1 uppercase (65-90)
+// 1 special (33-64)
+// //
+// How to generate it:
+// - any length >= 4
+// - any order 
+// - any symbols 
+
+function generatePassword(){
+    let password = "";
+    let constraints = [false,false,false,false]; 
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+      }
+
+    function generateRandomNumberWithDigits(digitNumber) {
+        var min = Math.pow(10, digitNumber- 1);
+        var max = Math.pow(10, digitNumber) - 1;
+        return getRndInteger(min, max);
+      }
+
+
+      function makeDigitSmallCaseLetter() {
+        let randomLetterNumber = getRndInteger(97, 122);
+        return String.fromCharCode(randomLetterNumber);
+      }
+
+      
+      function makeDigitUpperCaseLetter() {
+        let randomLetterNumber = getRndInteger(65, 90);
+        return String.fromCharCode(randomLetterNumber);
+      }
+
+      function makeDigitSymbol() {
+        let randomSymbolNumber = getRndInteger(33,64);
+        return String.fromCharCode(randomSymbolNumber);
+      }
+
+
+    // function countDigits(number) {
+    //     var numStr = number.toString();
+    //     return numStr.length;
+    // }
+
+    const paswordLength = getRndInteger(4, 10); 
+    let randomNumber = generateRandomNumberWithDigits(paswordLength);
+    constraints[0] = true;
+    console.log(randomNumber);
+
+    console.log(paswordLength);
+
+    password = randomNumber.toString();
+      password = makeDigitSmallCaseLetter() + password.substring(1);
+      password = password.substring(0, 1) + makeDigitUpperCaseLetter() + password.substring(2);
+      password = password.substring(0, 2) + makeDigitSymbol() + password.substring(3);
+
+    console.log("password: " + password);
+    return password;
+}
+
+
+let password = generatePassword();
+console.log(checkPassword(password));
