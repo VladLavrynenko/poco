318736e81d2ef83dc2385158ec3a509b96e69d9c
Vlad <vlad.lavrynenko@powercoders.org>
2024-05-01 16:56:25 +0200

    Set calculator, needs to add some constraints. (checking)

--- week-5/mn-algorithms/ex-retirement-calc/scripts.js
+++ week-5/mn-algorithms/ex-retirement-calc/scripts.js
@@ -7,3 +7,53 @@
  *  in the example that follows on the next slide.
  */
 
+
+function getBirthdate() { 
+    let age = 0;
+    let retirementAge = 0;
+    let isInputValid = false;
+
+    while(isInputValid == false){
+        age = prompt("What is your age?");
+
+        if (checkAge(age)) {
+            isInputValid = true;
+        } 
+    }
+        
+    isInputValid = false;
+    while (isInputValid == false) {
+        retirementAge = prompt("At what age would you like to retire?");
+        if (checkRetireAge(retirementAge)) {
+            isInputValid = true;
+        } 
+    }
+       
+
+    showRetirement(input);
+}
+
+function showRetirement(input) {
+    let leftYears = Number(retirementAge) - Number(age);
+    let date = new Date();
+    let todayYear = date.getFullYear();
+    let retireYear = todayYear + leftYears;
+    let msg = `You have ${leftYears} years left until you can retire.\nIt's ${todayYear}, so you can retire in ${retireYear}.` 
+    
+    alert(msg);
+    console.log(msg);
+}
+
+
+function isNumeric(value) {
+    return !isNaN(value);
+}
+
+function checkAge(age) {
+    return isNumeric(age) && (age >= 0 && age <= 120); 
+}
+
+function checkRetireAge(age) {
+    return isNumeric(age) && (age >= 50 && age <= 80); 
+}
+
